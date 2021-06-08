22d9ccd02e5818929a4432dc573206cae51915f3
MattMofDoom <matt.marlor@outlook.com>
2021-06-08 12:30:36 +1000

    Add a test project

---
+++ Lurgle.Logging.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using Lurgle.Logging;
+
+namespace Lurgle.Logging.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
