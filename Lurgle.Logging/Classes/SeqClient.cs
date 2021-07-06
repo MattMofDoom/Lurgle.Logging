b175e6744980b5eb52c6c397c54cd9112463b914
MattMofDoom <matt.marlor@outlook.com>
2021-07-06 16:34:06 +1000

    Add proxy settings for Serilog.Sinks.Seq

---
+++ Lurgle.Logging/Classes/SeqClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace Lurgle.Logging.Classes
+{
+    /// <summary>
+    ///     HTTP Client handler for Seq
+    /// </summary>
+    public class SeqClient : HttpClientHandler
+    {
+        /// <summary>
+        ///     Return a HTTP Client Handler for Seq
+        /// </summary>
+        /// <returns></returns>
+        public SeqClient()
+        {
+            if (Logging.Config.LogSeqUseProxy)
+            {
+                var proxy = new WebProxy
+                {
+                    Address = new Uri(Logging.Config.LogSeqProxyServer),
+                    BypassProxyOnLocal = Logging.Config.LogSeqBypassProxyOnLocal,
+                    BypassList = Logging.Config.LogSeqProxyBypass
+                        .Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(t => t.Trim()).ToArray(),
+                    UseDefaultCredentials = false
+                };
+
+                if (!string.IsNullOrEmpty(Logging.Config.LogSeqProxyUser) &&
+                    !string.IsNullOrEmpty(Logging.Config.LogSeqProxyPassword))
+                    proxy.Credentials = new NetworkCredential(Logging.Config.LogSeqProxyUser,
+                        Logging.Config.LogSeqProxyPassword);
+                else
+                    proxy.UseDefaultCredentials = true;
+
+                UseProxy = true;
+                Proxy = proxy;
+                UseDefaultCredentials = false;
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            }
+            else
+            {
+                UseProxy = false;
+                UseDefaultCredentials = true;
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            }
+        }
+    }
+}
